0b7631bee19656e64f3101deebc927ab67651a78
nikashs26 <nikash926@gmail.com>
2025-10-25 22:43:36 -0700

    landing page search working

--- src/context/MapCenterContext.jsx
+++ src/context/MapCenterContext.jsx
@@ -1,13 +1,17 @@
+// context/MapCenterContext.jsx
 import React, { createContext, useState } from 'react';
 
 export const MapCenterContext = createContext();
 
-export function MapCenterProvider({ children }) {
-  const [mapCenter, setMapCenter] = useState({ lat: 47.6061, lng: -122.4194 });
+export const MapCenterProvider = ({ children }) => {
+  const [mapCenter, setMapCenter] = useState({ 
+    lat: 47.6061, 
+    lng: -122.3328 // Default to Seattle
+  });
 
   return (
     <MapCenterContext.Provider value={{ mapCenter, setMapCenter }}>
       {children}
     </MapCenterContext.Provider>
   );
-}
+};
